1fe418503f11e5e9ee3a294d4daa5fae64b160b3
amisha-singh12 <singhamisha1572@gmail.com>
2025-08-16 08:39:20 +0530

    day 1 commit

---
+++ WEEK1 - CORE JAVA FUNDAMENTALS/programs/helloworld.java
@@ -0,0 +1,19 @@
+// 1. Class Definition
+
+// Every Java program must have at least one class. 
+// If the class is public, the filename must match the class name helloworld.java
+
+public class helloworld {
+
+// 2. main Method
+
+// public: Allows JVM to access the method from anywhere.
+//  static: Method can run without creating an object.
+// void: It doesn’t return any value.
+// String[] args: Accepts command-line arguments.
+    public static void main(String [] args)
+
+{
+    System.out.println("hello worls to this java development");
+}
+}
